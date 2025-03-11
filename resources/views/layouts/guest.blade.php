de4fe9498d24cd4d2c7ce21548d62f7fbec7d56b
Eric Byrne (Student) <N00231238@iadt.ie>
2025-03-11 13:01:27 +0000

    Still Working on login and sign up UI

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,7 +14,7 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans text-gray-900 antialiased">
+    <body class="login_signup_bg">
         <div class="signup_h1">
             <h1>Sign up to Finders Cleaners</h1>
         </div>
