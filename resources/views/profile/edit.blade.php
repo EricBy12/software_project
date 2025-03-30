3b21090be5ae7e602e127bcc4c1f2d1805ac87b8
Eric Byrne <n00231238@iadt.ie>
2025-03-30 00:34:47 +0000

    29/03/25

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,13 +1,13 @@
-<x-app-layout>
+<x-app-layout class="settings">
     <!-- <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Profile') }}
         </h2>
     </x-slot> -->
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="">
+    <div class="py-12 settings">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6 settings">
+            <div class="settings">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
                 </div>
@@ -16,6 +16,13 @@
             <div class="">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
+                </div>
+            </div>
+
+            <div>
+                <div class="">
+                    <label for="">Hide real name</label>
+                    <button class="">Yes</button>
                 </div>
             </div>
 
